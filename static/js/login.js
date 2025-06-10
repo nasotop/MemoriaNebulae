8c9b6586e044adb1d9c4d7fcb7662bafa25d13d4
nasotop <na.sotop@duocuc.cl>
2025-06-09 21:28:58 -0400

    fix
    corrige vista de admin y añade validación en js para redirigir a administrador

--- static/js/login.js
+++ static/js/login.js
@@ -1,51 +1,56 @@
 const _ui = {
-    form: "login_form",
-    mail: "user_mail",
-    password: "user_pass",
-    submit: "login_submit",
-}
+	form: "login_form",
+	mail: "user_mail",
+	password: "user_pass",
+	submit: "login_submit",
+};
 
-const pattern = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+const pattern =
+	/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
 window.addEventListener("load", () => {
-    document.getElementById(_ui.submit).addEventListener("click", login);
+	document.getElementById(_ui.submit).addEventListener("click", login);
 });
 
 const login = (event) => {
-    event.preventDefault();
-    const validations = {
-        mail: true,
-        password: true,
-    };
+	event.preventDefault();
+	const validations = {
+		mail: true,
+		password: true,
+	};
 
-    const mail = document.getElementById(_ui.mail);
-    const password = document.getElementById(_ui.password);
+	const mail = document.getElementById(_ui.mail);
+	const password = document.getElementById(_ui.password);
 
+	if (mail.value === "") {
+		validations.mail = false;
+		setValidation(mail, false, "Por favor, ingrese su correo electrónico");
+	} else if (!pattern.test(mail.value)) {
+		validations.mail = false;
+		setValidation(
+			mail,
+			false,
+			"Por favor, ingrese un correo electrónico válido"
+		);
+	} else {
+		validations.mail = true;
+		setValidation(mail, true, "");
+	}
 
-    if (mail.value === "") {
-        validations.mail = false;
-        setValidation(mail, false, "Por favor, ingrese su correo electrónico");
-    }
-    else if (!pattern.test(mail.value)) {
-        validations.mail = false;
-        setValidation(mail, false, "Por favor, ingrese un correo electrónico válido");
-    }
-    else {
-        validations.mail = true;
-        setValidation(mail, true, "");
-    }
+	if (password.value === "") {
+		validations.password = false;
+		setValidation(password, false, "Por favor, ingrese su contraseña");
+	} else {
+		validations.password = true;
+		setValidation(password, true, "");
+	}
 
-    if (password.value === "") {
-        validations.password = false;
-        setValidation(password, false, "Por favor, ingrese su contraseña");
-    } else {
-        validations.password = true;
-        setValidation(password, true, "");
-    }
+	const esAdmin =
+		mail.value === "admin@admin.cl" && password.value === "admin123";
 
-    if (Object.values(validations).every(value => value === true)) {
-        window.location.href = event.target.href;
-    }
+	if (Object.values(validations).every((value) => value === true)) {
+		window.location.href = esAdmin ? "index-admin.html" : event.target.href;
+	}
 };
 const setValidation = (element, isValid, errorMsg) => {
 	const validation_message = element.parentElement.querySelector(
